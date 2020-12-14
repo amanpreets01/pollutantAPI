7d46d9b357b2445baa4d128654da7b5227d1ad08
amanpreets01 <amanpreets879@gmail.com>
2020-12-15 00:29:48 +0530

    Changes

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -7,6 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 import os
+import polls.settings
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "polls.settings")
